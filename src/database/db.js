32ab89cbd01847e7214ef6350d5a237b98d61319
kinshuk-0 <kinshukguptaa@gmail.com>
2023-08-26 02:43:59 +0530

    null pointer exception handling

--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const sequelize = new sequelize_1.Sequelize(process.env.DB, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 33061
+    port: 3306
 });
 sequelize
     .authenticate()
