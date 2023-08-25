930512d67712816e6ff66707cd64dc8fdde592d7
kinshuk-0 <kinshukguptaa@gmail.com>
2023-08-26 01:49:15 +0530

    deployment error fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,7 +3,7 @@
 import { Sequelize } from 'sequelize'
 require('dotenv').config()
 
-const sequelize = new Sequelize('bitespeed_identity_reconciliation', process.env.DB_USERNAME!!, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB!!, process.env.DB_USERNAME!!, process.env.DB_PASSWORD, {
   host    : process.env.DB_HOST,
   dialect : 'mysql',
   port    :  33061
