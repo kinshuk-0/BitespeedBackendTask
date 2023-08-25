1dd432bc94b411973759ccd9f461c9589d4c52ba
kinshuk-0 <kinshukguptaa@gmail.com>
2023-08-26 02:08:03 +0530

    deployment error fix

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 require('dotenv').config();
-const sequelize = new sequelize_1.Sequelize('bitespeed_identity_reconciliation', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelize = new sequelize_1.Sequelize(process.env.DB, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
